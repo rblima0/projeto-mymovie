a4a6cfbe24926534e5d8decc866ea7647c8d5d06
Rodrigo Banci <rblima0@gmail.com>
2018-06-21 13:19:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Projeto MyMovies
+
+Projeto em desenvolvimento em breve mais informações...
+
+[Create React App](https://github.com/facebookincubator/create-react-app)
+[API](https://www.themoviedb.org/documentation/api)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import * as MovieAPI from './MovieAPI'
+import './App.css'
+
+class App extends React.Component {
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>Iniciando o projeto</h1>  
+      </section>
+    )
+  }
+}
+
+export default App
