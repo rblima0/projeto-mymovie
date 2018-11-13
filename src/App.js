6cad9d4c4482db3e274c66c6cc8bf9eb6e5c9f3a
Rodrigo Banci <rblima0@gmail.com>
2018-11-13 13:29:58 -0200

    Estrutura atualizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 // import * as MovieAPI from './MovieAPI'
+import Menu from './components/Menu';
+import Movie from './components/Movie';
 import './styles/css/App.css';
 
 class App extends React.Component {
@@ -9,9 +11,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <section>
-        <h1>Iniciando o projeto</h1>  
-      </section>
+      <div className="container">
+        <div className="row">
+            
+            {/* CHAMAR MENU */}
+            <Menu />
+        
+            <section className="col col-content">
+
+            {/* CHAMAR FILME */}
+            <Movie />
+    
+            </section>
+
+        </div>
+    </div>
     )
   }
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class Menu extends Component {
+
+    /* constructor(){
+
+    } */
+
+    render() {
+        return (
+            <section className="col col-sidebar">
+                <div className="menu-trigger" id="menu-trigger">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <nav className="menu-hidde" id="menu-hidde">
+                    <img src="img/mymovies-logo.svg" alt="Logo icon" />
+                    <span>Menu Principal</span>
+                    <ul>
+                        <li><a href="index.html">Lançamentos</a></li>
+                        <li><a href="#">Próximas estreias</a></li>
+                        <li><a href="#">Populares</a></li> 
+                        <li><a href="#">Bem avaliados</a></li>
+                        <li><a href="#">Pessoas</a></li>
+                    </ul>
+                    <span>Gêneros</span>
+                    <ul>
+                        <li><a href="#">Ação</a></li>
+                        <li><a href="#">Aventura</a></li>
+                        <li><a href="#">Animação</a></li>
+                        <li><a href="#">Romance</a></li>
+                        <li><a href="#">Drama</a></li>
+                        <li><a href="#">Comédia</a></li>
+                        <li><a href="#">Terror</a></li>
+                        <li><a href="#">Suspense</a></li>
+                    </ul> 
+                </nav>
+            </section>
+        );
+    }
+}
+
+/* props - guarda todos os parametros enviados para esse componente */
