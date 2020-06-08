541e78fa63016795a94a04a74116fd17068f83fb
Rodrigo Banci <rblima0@gmail.com>
2020-06-08 15:49:40 -0300

    fix: correction for routes

--- src/routes/InternalRoutes.tsx
+++ src/routes/InternalRoutes.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Route, Switch, Redirect } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import { Dashboard } from '../pages/Dashboard'
 
 export function InternalRoutes() {
   return (
     <Switch>
-      <Redirect from="/" to="/dashboard" exact />
       <Route path="/dashboard" component={Dashboard} />
-      <Redirect to="/not-found" />
     </Switch>
   )
 }
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import { Menu } from '../pages/Menu'
 
 export function Routes() {
   return (
     <Switch>
+      <Redirect from="/" to="/dashboard" exact />
       <Route component={Menu} path="/dashboard" />
+      <Redirect to="/not-found" />
     </Switch>
   )
 }
